0c5dd98e953d5208c58505bc11acb52ed7231624
Ahmed Khaled <ahmedkhaled11119999@gmail.com>
2020-12-15 02:29:37 +0200

    Added searchbar , finished available screen

--- lib/screens/browse_screen.dart
+++ lib/screens/browse_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:treat_min/widgets/doctor_card.dart';
+import 'package:treat_min/SearchBar.dart';
 
 class BrowseScreen extends StatelessWidget {
   static const routeName = '/browse';
@@ -8,45 +9,67 @@ class BrowseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: null,
-            //TODO:need to add functionality to pop the last screen in stack
-            // Navigator.pop(context, false);
-          ),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/icons/Tooth.png",
-                width: 25.0,
-                height: 25.0,
-              ),
-              SizedBox(
-                width: 5.0,
-              ),
-              Text(
-                "Dentist",
-                style: TextStyle(fontSize: 25.0),
-              ),
-            ],
-          ),
-          actions: [
-            IconButton(
-              icon: Image.asset("assets/icons/Filter.png"),
-              onPressed: () {
-                print("Hey!");
-              },
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: null,
+          //TODO:need to add functionality to pop the last screen in stack
+          // Navigator.pop(context, false);
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/icons/Tooth.png",
+              width: 25.0,
+              height: 25.0,
             ),
-            IconButton(
-              icon: Image.asset("assets/icons/Sort.png"),
-              onPressed: () {
-                print("Hey2!");
-              },
-            )
+            SizedBox(
+              width: 5.0,
+            ),
+            Text(
+              "Dentist",
+              style: TextStyle(fontSize: 25.0),
+            ),
           ],
         ),
-        body: DoctorCard());
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+            ),
+            onPressed: () {
+              showSearch(context: context, delegate: DataSearch());
+            },
+          ),
+          IconButton(
+            icon: Image.asset("assets/icons/Filter.png"),
+            onPressed: () {
+              print("Hey!");
+            },
+          ),
+          IconButton(
+            icon: Image.asset("assets/icons/Sort.png"),
+            onPressed: () {
+              print("Hey2!");
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          DoctorCard(),
+          DoctorCard(),
+          DoctorCard(),
+          DoctorCard(),
+          DoctorCard(),
+          DoctorCard(),
+          DoctorCard(),
+          DoctorCard(),
+          DoctorCard(),
+        ],
+        // scrollDirection: Axis.vertical,
+      ),
+    );
   }
 }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Settings Screen'));
+    return Center(child: Text("Settings Screen"));
   }
 }
